5845c7141b5226e469b7641980732f573e04b70f
Christopher Gonçalves <chrissgon.dev@gmail.com>
2024-04-01 16:14:54 +0100

    :bug: fix: adjust github link

--- src/components/Atom.GithubLink.tsx
+++ src/components/Atom.GithubLink.tsx
@@ -3,7 +3,7 @@ export default function AtomFigmaLink() {
     <a
       rel="noopener"
       aria-label="View the source code on Github"
-      href="https://github.com/chrissgon/perfectui"
+      href="https://github.com/chrissgon/rickandmorty"
       target="_blank"
     >
       <i className="bi-github text-2xl leading-none" />
